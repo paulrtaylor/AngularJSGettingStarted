fa5e3d62549c1d88086bbe030487cdf947f8a059
Paul Taylor <paultaylor@MacBookPro.local>
2015-04-10 20:43:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+(function () {
+
+    var app = angular.module("gitHubViewer", []);
+
+    var mainController = function ($scope, $http) {
+
+        // Options to search for: paulrtaylor, odetocode, angular
+        var onRepos = function(response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Unable to retrieve the data: " + reason.data.message;
+        };
+
+        var onUserComplete = function(response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        $scope.search = function(username) {
+
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        };
+
+        $scope.message = "Github Viewer";
+
+    };
+
+    app.controller("MainController", ["$scope", "$http", mainController]);
+
+}());
