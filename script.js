4c47f09a9e464cedb04deab2d31145d3e07ba0c2
Paul <paul_taylor@outlook.com>
2015-04-11 14:47:30 +0100

    Add a quick countown function to show how this can be done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
     var app = angular.module("gitHubViewer", []);
 
-    var mainController = function ($scope, $http) {
+    var mainController = function ($scope, $http, $interval) {
 
         // Options to search for: paulrtaylor, odetocode, angular
         var onRepos = function(response) {
@@ -20,6 +20,17 @@
                 .then(onRepos, onError);
         };
 
+        var decrementCountdown = function () {
+            $scope.countdown -=1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            }
+        };
+
+        var startCountdown = function () {
+            $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
         $scope.search = function(username) {
 
             $http.get("https://api.github.com/users/" + username)
@@ -29,9 +40,11 @@
         $scope.username = "angular";
         $scope.message = "Github Viewer";
         $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+        startCountdown();
 
     };
 
-    app.controller("MainController", ["$scope", "$http", mainController]);
+    app.controller("MainController", ["$scope", "$http", "$interval", mainController]);
 
 }());
